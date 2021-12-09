925845bb4f6c903932cd8cb6297d6a50e2d77829
SidM1811 <92666471+SidM1811@users.noreply.github.com>
2021-12-09 21:43:52 +0000

    Update model.py

--- model.py
+++ model.py
@@ -22,6 +22,8 @@ def avg(l):
 
 def dotProd(l1,l2):
     acc=0
+    if len(l1)!=len(l2):
+        raise IndexError
     for i in range(0,len(l1)):
         acc+=l1[i]*l2[i]
     return acc
@@ -45,12 +47,22 @@ def norm(l):
     list1.append(M)
     list1.append(m)
     return list1
-
+def norm1(l):
+    sum1=[]
+    for i in range(0,len(l[0])):
+        sum1.append(l[0][i])
+        sum1.append(l[0][i])
+    for i in range(0,len(l[0])):
+        for j in range(0,len(l)):
+            sum1[i]+=l[j][i]
+    for i in range(0,len(l[0])):
+        for j in range(0,len(l)):
+            l[j][i]-=sum1[i]
 def denorm(l1,index,val):
     Mi=l1[0][index]
     mi=l1[1][index]
     return val*(Mi-mi)+mi
-def train():
+def train(alpha):
     corr=[0,0,0,0,0,0]
     for i in range(0,len(rows)-1):
         act=rows[i+1][1]
@@ -66,29 +78,32 @@ def train():
 
 import csv
 rows=[]
-alpha=0.1#learning rate
 
-with open("PUT VALID FILEPATH", 'r', newline='') as csvfile:
+with open("ADD FILEPATH", 'r', newline='') as csvfile:
     rd=csv.reader(csvfile,delimiter=",")
     for row in rd:
         if row[0]=='date':
             continue
-        data=[float(row[2]),float(row[3]),float(row[4]),float(row[5]),float(row[6])]
-        rows.append(data)
+        if row[1]=='AAPL':
+            data=[float(row[2]),float(row[3]),float(row[4]),float(row[5]),float(row[6])]
+            rows.append(data)
 list1=norm(rows)
-trainVec=[0,0.25,0.25,0.25,0.25,0]
+trainVec=[0,0,0,0,0,0]
 def test():
     loss=0
     for i in range(0,len(rows)-1):
-        data=rows[i]
+        data=rows[i].copy()
+        data.insert(0,1.0)
         pred=dotProd(trainVec,data)
-        loss+=calcLoss(rows[i+1][2],pred)
-        #print(denorm(list1,1,pred),"\t\t",denorm(list1,1,data[1]),"\t\t",calcError(denorm(list1,1,pred),denorm(list1,1,data[1])))
+        print(denorm(list1,1,rows[i+1][1]),denorm(list1,1,pred))
+        loss+=calcLoss(denorm(list1,1,rows[i+1][1]),denorm(list1,1,pred))
     loss/=(len(rows)-1)
     print(loss)    
 def main():
-    for i in range(0,1):
-        train()
+    for epoch in range(1,21):
+        alpha=0.1/epoch
+        for i in range(0,100):
+            train(alpha)
     test()
         
 main()         
